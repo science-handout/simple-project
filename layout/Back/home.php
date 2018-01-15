76adac5627342dd5d66d17bc209183323c43a4ae
Mohamed Amr <dev.mohamedamr@gmail.com>
2018-01-15 19:17:48 +0200

    fix

---
+++ layout/Back/home.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * simple creation project
+ * @copyright Copyright (c) mohamed amr
+ * @license https://github.com/science-handout/simple-project/blob/master/LICENSE (MIT License)
+ * html file
+ */
+?>
+<?php if (!$_GET['action']): ?>
+<!--  select area  -->
+
+
+
+
+
+
+
+
+
+<?php elseif (($_GET['action'] == 'add')): ?>
+    <!--  add area  -->
+
+
+
+
+
+
+
+
+
+
+<?php elseif (($_GET['action'] == 'update')): ?>
+    <!--  update area  -->
+
+
+
+
+
+
+
+
+
+
+
+<?php elseif (($_GET['action'] == 'delete')): ?>
+    <!--  delete area  -->
+
+
+
+
+
+
+
+
+<?php endif; ?>
--- layout/Back/nav.php
+++ layout/Back/nav.php
@@ -1,3 +1,3 @@
 
-<li class='nav-item'><a class='nav-link' href='home.php'>home</a></li>
 
+<li class="nav-item" data-toggle="tooltip" data-placement="right" title="home"><a class='nav-link'  href='home.php'><i class="fa fa-fw fa-angle-left"></i><span class="nav-link-text">home</span>  </a></li>
